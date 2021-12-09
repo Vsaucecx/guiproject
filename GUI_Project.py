63385584e5d9eb8db0ed4dbd37c420f921e60790
02juan04 <romerojuan202@gmail.com>
2021-12-08 18:56:42 -0800

    two buttons work
    I made it so that it deletes the buttons after we click one so that we put the questions. We can call the buttons back later. Maybe at the end of the quiz we can put a button that says restart or something idk

--- GUI_Project.py
+++ GUI_Project.py
@@ -1,26 +1,46 @@
 #Gavin Chan
 import tkinter as tk
+from tkinter.constants import CENTER
 
 gui = tk.Tk()
 gui.title("GUI Project")
 gui.geometry("1200x800")
 gui.configure(bg='grey')
 
+def startButtonClick():
+    startbutt.place_forget()
+    creditbutt.place_forget()
+    instructionbutt.place_forget()
+
+    question1 = tk.Label(gui, text = "This is question 1")
+    question1.place()
+
+def intructionsButtonClick():
+    startbutt.place_forget()
+    creditbutt.place_forget()
+    instructionbutt.place_forget()
+    title.place_forget()
+    
+    title.configure(text='Instructions')
+    title.pack()
+    
+    intructions = tk.Label(gui, text="These are the intructions", )
+    intructions.place(x=100, y=100)
 
 
 title = tk.Label(gui, text="THE IMPOSSIBLE QUIZ", font=("times",50,"italic"))
-title.place(x=250, y=75)
+title.place(x=250, y=25)
 
 credit = tk.Label(gui, text="By: Gavin Chan \nDaniel Bueno \nJuan Romero", font=("times",12,"italic"))
 credit.place(x=1075, y=725)
 
-startbutt = tk.Button(gui, text='START', font=("times",20,"italic"),width=30, height=4)
+startbutt = tk.Button(gui, text='START', font=("times",20,"italic"),width=30, height=4, command=startButtonClick)
 startbutt.place(x=100, y=300)
 #this is just a test idk how this works
 creditbutt = tk.Button(gui, text='CREDITS', font=("times",20,"italic"),width=30, height=4)
 creditbutt.place(x=100, y=500)
 # test number 2
-instructionbutt = tk.Button(gui, text='INSTRUCTIONS', font=("times",20,"italic"),width=30, height=4)
+instructionbutt = tk.Button(gui, text='INSTRUCTIONS', font=("times",20,"italic"),width=30, height=4 , command = intructionsButtonClick)
 instructionbutt.place(x=645, y=300)
 
 gui.mainloop()
